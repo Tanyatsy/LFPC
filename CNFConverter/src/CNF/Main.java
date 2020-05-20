f60fe067ae66b3bf2433770cc5d634a645444f9c
tanya <tanyatsy00@gmail.com>
2020-05-20 13:58:36 +0000

    add RG to CNF converter

---
+++ CNFConverter/src/CNF/Main.java
@@ -0,0 +1,39 @@
+package CNF;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        GrammarRules grammar = new GrammarRules();
+//Write absolute path here!!
+            if (grammar.importFile("C:\\Users\\tanya\\Documents\\University\\Program Language\\Labs\\Labs\\RGtoCNF\\CNF\\grammar.txt")) {
+                System.out.println(grammar.toString());
+
+            grammar.printGrammar();
+
+            // Check recursive initial symbol
+            if (grammar.verifyInitialRecursion()) {
+                grammar.removeInitialRecusion();
+            } else
+                System.out.println("No initial recursive rule");
+
+            // Check Labda Symbols
+            if (grammar.verifyLambdaRule()) {
+                grammar.removeLambda();
+            } else
+                System.out.println("No Lambda rule");
+
+            // Check chain rule
+            if (grammar.verifyChainRule()) {
+                grammar.removeChainRule();
+            } else
+                System.out.println("No chain rule");
+
+
+            grammar.printGrammar();
+        }
+
+    }
+
+}
